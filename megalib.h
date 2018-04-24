ee9c5a47cc7b469812c90ecf474feb5dab772877
Tai <37852567+Taiguz@users.noreply.github.com>
2018-04-24 18:37:36 -0300

    Update megalib.h

--- megalib.h
+++ megalib.h
@@ -1,8 +1,18 @@
 #ifndef _MEGALIB_
 #define _MEGALIB_
 
+
+#ifdef _WIN32
+#define SIS 1
+#endif
+
+#ifdef linux
+#define SIS 0
+#endif
+
 // Criando constantes
 #define TAM_CARTA 17
+#define TAM_PADRAO 5
 // Criando estrtura que irá armazenar os caracteres de uma linha para todo o resto dos gráficos.
 typedef struct {
 
@@ -19,7 +29,7 @@ typedef struct {
 	int atk;
 
 	int def;
-// Modo : 1 - Ataque, 2 - Defesa;
+// Modo : 0 - Ataque, 1 - Defesa;
 	int modo;
 
 	char nome[50];
@@ -32,6 +42,20 @@ typedef struct {
 
 	LINHA nome_menu[1];
 } CARTA;
+
+typedef struct {
+
+	int modo;
+
+	int atk;
+
+	int def;
+
+	int atacou;
+
+	LINHA inf;
+
+} TEMP_CARTA;
 
 typedef struct {
 
@@ -56,17 +80,23 @@ typedef struct {
 } MENU;
 
 // Definindo os Headers das funções
-int Comando(char*);
-void DesenharMesa(int*, int*, int*, CARTA*, HUD*);
-void DesenharMenu(int, HUD*);
+int Comando(char*,int*);
+void DesenharMesa(int*, int*, int*, CARTA*, HUD*, TEMP_CARTA*,TEMP_CARTA*);
+void DesenharMenu(int, HUD*, CARTA*, int*, int*, int*, int*);
 void DesenharTela(int, TELA*);
 void CriarHUD(HUD*);
 void CriarCartas(CARTA*);
 void CriarTelas(TELA*);
+void LimparTela(int);
 void LimparMenu(HUD*);
 void MudarTela(int, int*, int*, int*);
-void MudarHud(int, HUD*, CARTA*, int*, int*, int*);
+void SumonarCarta(int,int,int*, int*, TEMP_CARTA*, CARTA*);
+void Controle(int, HUD*, CARTA*, int*, int*, int*, int*, int*, TEMP_CARTA*,TEMP_CARTA*, int*);
 void TransicaoTela(int, int*, int*, TELA*);
-void IniciarJogo(int*, int*, int*, int*);
+void IniciarJogo(int*, int*, int*, int*,int*, TEMP_CARTA*, TEMP_CARTA*,CARTA*);
+int AtacarCarta(int, int, TEMP_CARTA*,TEMP_CARTA*, int*, int*,int*);
+void TrocarModo(int,TEMP_CARTA*);
+
+
 
 #endif
